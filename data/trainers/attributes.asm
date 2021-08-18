21cc8222a1f490ecbea2376758c1c921c50ad21d
MajorAgnostic <lostprophecy14@live.com>
2021-08-18 15:17:32 -0400

    Pre-1.0 changes except generic Kanto trainers

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -200,7 +200,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Teacher
-	db FULL_RESTORE, MAX_POTION ; items
+	db NO_ITEM, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
